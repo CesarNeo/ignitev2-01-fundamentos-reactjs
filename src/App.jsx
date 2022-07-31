6e9475efd0672aa7e60d829ebe7115a15bcf3d41
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-07-31 16:42:36 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 
 function App() {
   return (
     <>
+      <Header />
       <Post
         author="Cesar Emmanuel"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et sapiente, amet voluptatibus officia distinctio mollitia repellat illo rem, tempore labore dignissimos dolor? Recusandae corporis tempora excepturi nam nihil iure dolor."
