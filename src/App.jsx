a081ade28e378255a7fa80bb5399ba4cf164203b
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-08-01 18:51:18 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './Post'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -7,10 +10,17 @@ function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Cesar Emmanuel"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et sapiente, amet voluptatibus officia distinctio mollitia repellat illo rem, tempore labore dignissimos dolor? Recusandae corporis tempora excepturi nam nihil iure dolor."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Cesar Emmanuel"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et sapiente, amet voluptatibus officia distinctio mollitia repellat illo rem, tempore labore dignissimos dolor? Recusandae corporis tempora excepturi nam nihil iure dolor."
+          />
+        </main>
+      </div>
     </>
   )
 }
