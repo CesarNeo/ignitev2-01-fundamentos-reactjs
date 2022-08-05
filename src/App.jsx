4ae881d3eccd512bbef1dc7eaf60f51b211857fd
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-08-05 19:32:45 -0300

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/cesarneo.png',
+      name: 'Cesar Emmanuel',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  }
+]
+
 function App() {
   return (
     <>
@@ -15,15 +54,14 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Cesar Emmanuel"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et sapiente, amet voluptatibus officia distinctio mollitia repellat illo rem, tempore labore dignissimos dolor? Recusandae corporis tempora excepturi nam nihil iure dolor."
-          />
-
-          <Post
-            author="Cesar Emmanuel"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et sapiente, amet voluptatibus officia distinctio mollitia repellat illo rem, tempore labore dignissimos dolor? Recusandae corporis tempora excepturi nam nihil iure dolor."
-          />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
