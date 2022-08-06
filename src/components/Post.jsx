4ca1ed5c4a84f19b2679a8b53cf42ded747cc674
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-08-06 16:57:24 -0300

    Comunicação entre componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -37,6 +37,8 @@ export function Post({ author, content, publishedAt }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(comment) {}
+
   return (
     <article className={styles.post}>
       <header>
@@ -88,7 +90,11 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => (
-          <Comment key={comment} content={comment} />
+          <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={deleteComment}
+          />
         ))}
       </div>
     </article>
